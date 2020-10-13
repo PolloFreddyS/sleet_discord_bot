2d037c4fff54c2e30f9e537b4bbeb9565b4c63f8
PolloFreddyS <36654766+PolloFreddyS@users.noreply.github.com>
2020-10-13 13:49:41 -0400

    Update server_management.py

--- bot_functions/server_management.py
+++ bot_functions/server_management.py
@@ -17,3 +17,10 @@ def unmute_members(context):
             if all_channel_members[i].voice is not None:
                 if all_channel_members[i].voice.channel == channel_requested:
                     yield all_channel_members[i].edit(mute=False)
+
+async def pollo(ctx):
+ user_request = ctx.author
+ dst_channel = "la habitacion del pollo"
+ 
+ lst = member.move_to(dst_channel)
+ await asyncio.gather(*lst)
